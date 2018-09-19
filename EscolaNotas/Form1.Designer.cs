6dcc8b0f212e2cc6e5daa5d13a39ff44b4824444
Renato Costa <renato_da_costa2000@yahoo.com.br>
2018-09-19 10:05:00 -0300

    Windows forms estudo

---
+++ EscolaNotas/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace EscolaNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dgvAlunos = new System.Windows.Forms.DataGridView();
+            this.NOME = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOTA1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOTA2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NOTA3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MEDIA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lblNota3 = new System.Windows.Forms.Label();
+            this.btnresultado = new System.Windows.Forms.Button();
+            this.btnMediaTotal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAlunos
+            // 
+            this.dgvAlunos.AllowUserToAddRows = false;
+            this.dgvAlunos.AllowUserToDeleteRows = false;
+            this.dgvAlunos.AllowUserToResizeColumns = false;
+            this.dgvAlunos.AllowUserToResizeRows = false;
+            this.dgvAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlunos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NOME,
+            this.NOTA1,
+            this.NOTA2,
+            this.NOTA3,
+            this.MEDIA});
+            this.dgvAlunos.Location = new System.Drawing.Point(12, 73);
+            this.dgvAlunos.Name = "dgvAlunos";
+            this.dgvAlunos.ReadOnly = true;
+            this.dgvAlunos.RowHeadersVisible = false;
+            this.dgvAlunos.Size = new System.Drawing.Size(742, 468);
+            this.dgvAlunos.TabIndex = 0;
+            // 
+            // NOME
+            // 
+            this.NOME.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NOME.HeaderText = "Nome";
+            this.NOME.Name = "NOME";
+            this.NOME.ReadOnly = true;
+            // 
+            // NOTA1
+            // 
+            this.NOTA1.HeaderText = "Nota1";
+            this.NOTA1.Name = "NOTA1";
+            this.NOTA1.ReadOnly = true;
+            // 
+            // NOTA2
+            // 
+            this.NOTA2.HeaderText = "Nota 2";
+            this.NOTA2.Name = "NOTA2";
+            this.NOTA2.ReadOnly = true;
+            // 
+            // NOTA3
+            // 
+            this.NOTA3.HeaderText = "Nota 3";
+            this.NOTA3.Name = "NOTA3";
+            this.NOTA3.ReadOnly = true;
+            // 
+            // MEDIA
+            // 
+            this.MEDIA.HeaderText = "Média";
+            this.MEDIA.Name = "MEDIA";
+            this.MEDIA.ReadOnly = true;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(12, 27);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(263, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(295, 27);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 1;
+            this.txtNota1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNota1_KeyPress);
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(415, 27);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 2;
+            this.txtNota2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNota1_KeyPress);
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(535, 27);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 20);
+            this.txtNota3.TabIndex = 3;
+            this.txtNota3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNota1_KeyPress);
+            // 
+            // btnadd
+            // 
+            this.btnadd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnadd.Location = new System.Drawing.Point(654, 24);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(101, 23);
+            this.btnadd.TabIndex = 4;
+            this.btnadd.Text = "Adicionar";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(13, 8);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 6;
+            this.lblNome.Text = "Nome";
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Location = new System.Drawing.Point(292, 8);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(45, 13);
+            this.lblNota1.TabIndex = 7;
+            this.lblNota1.Text = "Nota 1 :";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Location = new System.Drawing.Point(412, 9);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(45, 13);
+            this.lblNota2.TabIndex = 8;
+            this.lblNota2.Text = "Nota 2 :";
+            // 
+            // lblNota3
+            // 
+            this.lblNota3.AutoSize = true;
+            this.lblNota3.Location = new System.Drawing.Point(532, 8);
+            this.lblNota3.Name = "lblNota3";
+            this.lblNota3.Size = new System.Drawing.Size(45, 13);
+            this.lblNota3.TabIndex = 9;
+            this.lblNota3.Text = "Nota 3 :";
+            // 
+            // btnresultado
+            // 
+            this.btnresultado.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnresultado.Location = new System.Drawing.Point(12, 552);
+            this.btnresultado.Name = "btnresultado";
+            this.btnresultado.Size = new System.Drawing.Size(101, 23);
+            this.btnresultado.TabIndex = 5;
+            this.btnresultado.Text = "Resultado Geral";
+            this.btnresultado.UseVisualStyleBackColor = true;
+            this.btnresultado.Click += new System.EventHandler(this.btnresultado_Click);
+            // 
+            // btnMediaTotal
+            // 
+            this.btnMediaTotal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMediaTotal.Location = new System.Drawing.Point(654, 552);
+            this.btnMediaTotal.Name = "btnMediaTotal";
+            this.btnMediaTotal.Size = new System.Drawing.Size(101, 23);
+            this.btnMediaTotal.TabIndex = 6;
+            this.btnMediaTotal.Text = "Média dos Alunos";
+            this.btnMediaTotal.UseVisualStyleBackColor = true;
+            this.btnMediaTotal.Click += new System.EventHandler(this.btnMediaTotal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 587);
+            this.Controls.Add(this.btnMediaTotal);
+            this.Controls.Add(this.btnresultado);
+            this.Controls.Add(this.lblNota3);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.dgvAlunos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Sistema de Notas";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAlunos;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblNota1;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lblNota3;
+        private System.Windows.Forms.Button btnresultado;
+        private System.Windows.Forms.Button btnMediaTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOME;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOTA1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOTA2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NOTA3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MEDIA;
+    }
+}
+
---
+++ EscolaNotas/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscolaNotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Método que limpa os dados
+        /// </summary>
+        void limpadados()
+        {
+            txtNome.Text = string.Empty;
+            txtNota1.Text = string.Empty;
+            txtNota2.Text = string.Empty;
+            txtNota3.Text = string.Empty;
+           
+        }
+        /// <summary>
+        /// Botão que inseri as notas do aluno
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            if (validaDados())
+            {
+                //Cálculo da média
+                decimal nota1 = Convert.ToDecimal(txtNota1.Text);
+                decimal nota2 = Convert.ToDecimal(txtNota2.Text);
+                decimal nota3 = Convert.ToDecimal(txtNota3.Text);
+                decimal media = (nota1 + nota2 + nota3) / 3;
+                media = decimal.Round(media, 2);// Irá mostrar a média com 2 nºs apenas, arredondando
+
+                //Adicionando no DataGriedView 
+
+                dgvAlunos.Rows.Add();//Adicionamos a linha do dvgAlunos
+                                     //dgvAlunos.Rows[posicao].Cells["Nome da Coluna"].Value = valor;
+                                     //posicao=> última a ser inserida pois sempre começa do zero,ex: 1ª linha = valor 0 => nº de linhas -1 = posição
+                                     //Nome da Coluna=> igual ao NAME da edição das colunas obs:media é a variável estipulada aqui
+
+                dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["NOME"].Value = txtNome.Text;
+                dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["NOTA1"].Value = txtNota1.Text;
+                dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["NOTA2"].Value = txtNota2.Text;
+                dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["NOTA3"].Value = txtNota3.Text;
+                dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["MEDIA"].Value = media;
+
+                //Colorindo as colunas
+                if (media < 60)
+                {
+                    dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["MEDIA"].Style.BackColor = Color.Red;
+                    dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["MEDIA"].Style.SelectionBackColor = Color.Red;
+                }
+                else
+                {
+                    dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["MEDIA"].Style.BackColor = Color.Green;
+                    dgvAlunos.Rows[dgvAlunos.Rows.Count - 1].Cells["MEDIA"].Style.SelectionBackColor = Color.Green;
+                }
+                // Chamar método que limpa dados
+                limpadados();
+            }
+        }
+        /// <summary>
+        /// Método que valida uma nota preenchida
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        private bool validaNumero(string numero)
+        {
+            //tenta converter em decimal 
+            try
+            {
+                Convert.ToDecimal(numero);
+                return false;
+            }
+            //caso não consiga exibe msg p/ usuário
+            catch (Exception)
+            {
+                MessageBox.Show("Digite uma nota válida!");
+                return true;
+            }
+        }
+        /// <summary>
+        /// Método que valida dados
+        /// </summary>
+        /// <returns></returns>
+        private bool validaDados()
+        {
+            //verifica validade do nome
+            if (txtNome.Text.Replace(" ","")==string.Empty)//replace =>espaço vazio " "  por(,)   ""
+            {
+                MessageBox.Show("Digite um nome válido");
+                txtNome.Focus();//volta com o cursor para o nome
+                return false;
+            }
+            //verifica validade da nota
+           else if (validaNumero(txtNota1.Text.Replace(",",".")))
+            {
+                MessageBox.Show("Digite uma nota válida !");
+                txtNota1.Focus();
+                return false;
+            }
+            else if (validaNumero(txtNota2.Text.Replace(",", ".")))
+            {
+                MessageBox.Show("Digite uma nota válida !");
+                txtNota2.Focus();
+                return false;
+            }
+            else if (validaNumero(txtNota3.Text.Replace(",", ".")))
+            {
+                MessageBox.Show("Digite uma nota válida !");
+                txtNota3.Focus();
+                return false;
+            }
+            else if(Convert.ToDecimal(txtNota1.Text.Replace("," , "."))>100|| Convert.ToDecimal(txtNota1.Text.Replace(",", ".")) <0)
+            {
+                MessageBox.Show("Digite uma nota válida !");
+                txtNota1.Focus();
+                return false;
+            }
+            else if (Convert.ToDecimal(txtNota2.Text.Replace(",", ".")) > 100 || Convert.ToDecimal(txtNota2.Text.Replace(",", ".")) < 0)
+            {
+                MessageBox.Show("Digite uma nota válida !");
+                txtNota2.Focus();
+                return false;
+            }
+            else if (Convert.ToDecimal(txtNota3.Text.Replace(",", ".")) > 100 || Convert.ToDecimal(txtNota3.Text.Replace(",", ".")) < 0)
+            {
+                MessageBox.Show("Digite uma nota válida !");
+                txtNota3.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        /// <summary>
+        /// Botão que mostra o resultado geral
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnresultado_Click(object sender, EventArgs e)
+        {
+            int qtdAprovados = 0;
+            int qtdReprovados = 0;
+
+            for (int i = 0; i < dgvAlunos.Rows.Count; i++)// verificará em cada linha se foi aprovado ou reprovado
+            {
+                if (Convert.ToDecimal(dgvAlunos.Rows[i].Cells["MEDIA"].Value)<60)//percorre o grid
+                {
+                    qtdReprovados++;
+                }
+                else
+                {
+                    qtdAprovados++;
+                }
+            }
+            MessageBox.Show(qtdReprovados + " Aluno(s) reprovado(s)\n  " + qtdAprovados + " Aluno(s) Aprovado(s) ");
+        }
+        /// <summary>
+        /// Evento que mostra a média geral
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMediaTotal_Click(object sender, EventArgs e)
+        {
+            decimal mediaGeral = 0;
+            if (dgvAlunos.Rows.Count>0)   //passar pelas linhas do grid obs: tendo pelo menos um aluno ou msg
+            {  
+                for (int i = 0; i < dgvAlunos.Rows.Count; i++)
+            {
+                mediaGeral += Convert.ToDecimal(dgvAlunos.Rows[i].Cells["MEDIA"].Value);
+            }
+            //mediaGeral = mediaGeral / dgvAlunos.Rows.Count;
+            MessageBox.Show(" A média geral dos alunos é  :   " + decimal.Round(mediaGeral / dgvAlunos.Rows.Count,2));
+            }
+            else
+            {
+                MessageBox.Show("Não há alunos inseridos ");
+            }
+            
+        }
+
+        private void txtNota1_KeyPress(object sender, KeyPressEventArgs e)//selecionei as 3 cxs e criei o evento p/tds
+        {
+            if (!char.IsNumber(e.KeyChar)&& !char.IsPunctuation(e.KeyChar))//dif. de nº e de pontuação cancela a ação
+            {
+                //Cancela ação
+                e.Handled = true;//handled=manipulado
+            }
+        }
+    }
+}
